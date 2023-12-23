93c1075fd6fa5438ade59b9e63feffcdd0a42a26
hakanihat <62997624+hakanihat@users.noreply.github.com>
2023-12-24 01:21:24 +0200

    init project

---
+++ src/test/java/com/myproject/employeepairfinder/EmployeepairfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.employeepairfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeepairfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
