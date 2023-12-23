4b226bf31b6f619721ae93b94ba1badc040061a7
hakanihat <62997624+hakanihat@users.noreply.github.com>
2023-12-24 01:21:57 +0200

    Create EmployeepairfinderApplication.java

---
+++ src/main/java/com/myproject/employeepairfinder/EmployeepairfinderApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.employeepairfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepairfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepairfinderApplication.class, args);
+	}
+
+}
